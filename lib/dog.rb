03f758a2da29a6bbdde1068320cad06d75b08a83
Anthony Risch <rischthefisch@gmail.com>
2020-02-06 23:52:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  def name=(doggo_name)
+    @name = doggo_name
+  end
+
+def name
+  @name
+end
+
+def breed=(pupper)
+  @breed = pupper
+end
+
+def breed
+  @breed
+  end
+end
+
+
+fido = Dog.new 
+fido.name = "Fido"
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  
+def name=(who_you)
+    @name = who_you
+ end
+  
+def name
+    @name 
+ end
+
+def job=(what_do)
+  @job = what_do
+ end
+ 
+ def job
+   @job
+  end
+ 
+end
+
